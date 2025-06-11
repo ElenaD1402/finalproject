24bc8d19c1f0b40d93092f16b528d8ec1c6015d1
ElenaD1402 <sonne1402@gmail.com>
2025-06-12 02:11:55 +0300

    Updated Configuration, Jenkinsfile
    Updated Configuration, Jenkinsfile

--- FinalProject/src/main/java/org/elena/finalproject/webDriver/Configuration.java
+++ FinalProject/src/main/java/org/elena/finalproject/webDriver/Configuration.java
@@ -26,6 +26,8 @@ public class Configuration {
         properties = new Properties();
         try {
             properties.load(new FileInputStream("src/main/resources/project.properties"));
+            properties.putAll(System.getenv());
+            properties.putAll(System.getProperties());
         } catch (IOException ex) {
             System.out.println("Cannot read the properties");
             logger.error("Cannot read the properties");
